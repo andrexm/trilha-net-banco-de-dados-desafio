1e6b4e97c373d3a52aae6272fa13d0417e97b202
André Nascimento <andre.neospace@gmail.com>
2025-08-13 18:24:13 -0300

    feat: problem 07

--- Solucoes.sql
+++ Solucoes.sql
@@ -21,3 +21,7 @@ GO
 --- 06
 SELECT Nome, Ano, Duracao FROM Filmes WHERE Duracao > 100 AND Duracao < 150 ORDER BY Duracao
 GO
+
+--- 07
+SELECT COUNT(*) Quantidade FROM Filmes GROUP BY Ano ORDER BY SUM(Duracao) DESC
+GO
